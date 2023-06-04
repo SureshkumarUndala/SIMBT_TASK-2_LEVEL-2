ac5657f1fbba78720cdc58b82ed957b3e6cb74b6
Usk3210 <usk3210@gmail.com>
2023-06-04 18:30:54 +0530

    secondcommit

--- index.js
+++ index.js
@@ -13,8 +13,5 @@ mongoose.connect(process.env.mongodburl).then(()=>console.log("db connected"))
 app.use(express.json())
 app.use("/api/v1", Authentication)
 app.use("/api/v1", blogRoute)
-app.use("*",(req,res)=>{
-    res.send("Route Not Found")
 
-})
 app.listen(process.env.port,()=>console.log(`serever started at ${process.env.port}`))
